96e78813c5d7afd32adb7b5af246977866a102f6
Michał <michal.gniadek@microsoft.wsei.edu.pl>
2025-05-23 22:26:21 +0200

    Style update

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
   const hamburger = document.getElementById('hamburger');
   const navbar = document.getElementById('navbar');
 
+  const navlink= document.querySelectorAll('.nav-link');
+  
   hamburger.addEventListener('click', () => {
     navbar.classList.toggle('active');
     hamburger.classList.toggle('active');
+  });
+
+  navlink.forEach(link => {
+    link.addEventListener('click', () => {
+      navbar.classList.remove('active');
+      hamburger.classList.remove('active');
+    });
   });
